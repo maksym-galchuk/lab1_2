0b222dcfc57d141e9893cc6df1fb01dd8db16b02
Maksym Galchuk <maksymgalch@gmail.com>
2023-02-28 14:02:39 +0200

    Rectangle class prototype

--- cpp/Rectangle_lib/Rectangle.h
+++ cpp/Rectangle_lib/Rectangle.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <string>
+
+class Rectangle{
+private:
+    double x1, y1, x2, y2;
+public:
+    Rectangle(const Rectangle& r);
+    Rectangle(double x1, double y1, double x2, double y2);
+
+    double getX1();
+    double getY1();
+    double getX2();
+    double getY2();
+
+    void setX1(double x1);
+    void setY1(double y1);
+    void setX2(double x2);
+    void setY2(double y2);
+
+    double width();
+    double height();
+    double area();
+    double perimeter();
+    bool isRectangle();
+
+    std::String toString();
+};
